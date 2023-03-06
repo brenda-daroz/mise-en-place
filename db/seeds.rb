4daa4699b620d3bd57ccf6428697f43d659e3fe9
Brenda Daroz <brenda.daroz@gmail.com>
2023-03-06 16:28:06 +0100

    start with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts "Destroying recipes"
+Recipe.destroy_all
+
+API_KEY = ENV.fetch["API_KEY"]
+
+url = "https://api.spoonacular.com/recipes/random?number=10?apiKey=#{API_KEY}"
+response_serialized = URI.open(url).read
+response = JSON.parse(response_serialized)
+
+puts response
