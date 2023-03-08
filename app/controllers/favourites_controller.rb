452e0c386bfd821abd6bb17ef1c1d0f226a3e943
Frank Jurettko <f.jurettko@gmx.de>
2023-03-08 14:14:30 +0100

    favor

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,27 @@
 class FavouritesController < ApplicationController
+
+
+
+  def show
+
+      @favorites  = Favorite.where(user_id: current_user.id)
+
+  end
+
+
+  end
+
+  Recipe.all.each do | recipe |
+    favo = Favorite.new(user_id: user.id, recipe_id: recipe.id )
+    favo.save!
+  end
+
+
+  def create
+    favo = Favorite.new
+    favo.user = current_user
+    favo.recipe = Recipe.find(params[:recipe_id])
+    favo.save
+  end
+
 end
