24c616891d26e2a9d317b4fece00556dcddbe44e
Brenda Daroz <brenda.daroz@gmail.com>
2023-03-10 15:29:22 +0100

    fix button selection

---
+++ app/javascript/controllers/active_button_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="active-button"
+export default class extends Controller {
+  static targets = ["change"]
+  static classes = ["active"]
+  connect() {
+    console.log("hello from change button")
+    // console.log(this.changeTarget)
+  }
+
+  //   changeClass() {
+  //     // console.log(this.changeTarget)
+  //     this.element.classList.add(this.activeClass)
+  //   }
+}
