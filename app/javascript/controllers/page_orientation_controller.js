a89c133bac106eb7bb2d0e4e64063184e7694e4a
Brenda Daroz <brenda.daroz@gmail.com>
2023-03-14 09:43:50 +0100

    halfway done, needs more work

--- app/javascript/controllers/page_orientation_controller.js
+++ app/javascript/controllers/page_orientation_controller.js
@@ -4,22 +4,33 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["fullscreen"]
   connect() {
-    this.fullscreenTarget.addEventListener("fullscreenchange", () => {
+
+    const x = () => {
+      console.log("qualquer coisa")
       if (document.fullscreenElement) {
         this.fullscreenTarget.classList.remove('cooking-mode-hidden')
       } else
         this.fullscreenTarget.classList.add('cooking-mode-hidden')
-    })
+    }
+
+    this.fullscreenTarget.addEventListener("fullscreenchange", x)
+
+    this.fullscreenTarget.addEventListener("onwebkitfullscreenchange", x)
   }
 
   changeOrientation() {
     console.log("hello from stimulus orientation")
-
+    let isIOS = /iPad|iPhone|iPod/.test(navigator.platform) ||
+      (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
+    console.log(isIOS)
+    if (isIOS) {
+      window.location = this.element.dataset.route
+    }
     if (this.fullscreenTarget.requestFullscreen) this.fullscreenTarget.requestFullscreen()
-    // else if (this.fullscreenTarget.webkitRequestFullscreen) this.fullscreenTarget.webkitRequestFullscreen();
-    // else if (this.fullscreenTarget.mozRequestFullScreen) this.fullscreenTarget.mozRequestFullScreen();
-    // else if (this.fullscreenTarget.msRequestFullscreen) this.fullscreenTarget.msRequestFullscreen();
-    // else if (this.fullscreenTarget.webkitEnterFullscreen) this.fullscreenTarget.webkitEnterFullscreen();
+    else if (this.fullscreenTarget.webkitRequestFullscreen) this.fullscreenTarget.webkitRequestFullscreen();
+    else if (this.fullscreenTarget.mozRequestFullScreen) this.fullscreenTarget.mozRequestFullScreen();
+    else if (this.fullscreenTarget.msRequestFullscreen) this.fullscreenTarget.msRequestFullscreen();
+    else if (this.fullscreenTarget.webkitEnterFullscreen) this.fullscreenTarget.webkitEnterFullscreen();
     screen.orientation.lock("landscape")
   }
 }
