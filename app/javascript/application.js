d153a0b8b5b2e08115825226861abe17fb8319a3
Jelena Lufen <jelena.lufen@web.de>
2023-03-16 12:18:35 +0100

    x buttons turned into icon, when clicked page stays

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,31 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+import * as Turbo from "@hotwired/turbo"
+
+Turbo.scroll = {};
+
+document.addEventListener("turbo:load", ()=> {
+
+  const elements = document.querySelectorAll("[data-turbolinks-scroll]");
+
+  elements.forEach(function(element){
+
+    element.addEventListener("click", ()=> {
+      Turbo.scroll['top'] = document.scrollingElement.scrollTop;
+    });
+
+    element.addEventListener("submit", ()=> {
+      Turbo.scroll['top'] = document.scrollingElement.scrollTop;
+    });
+
+  });
+
+  if (Turbo.scroll['top']) {
+    document.scrollingElement.scrollTo(0, Turbo.scroll['top']);
+  }
+
+  Turbo.scroll = {};
+});
+
