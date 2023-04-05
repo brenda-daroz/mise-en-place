cdc1516c4f379a6a8ab3f466aba2d284595a7133
Brenda Daroz <brenda.daroz@gmail.com>
2023-04-05 12:00:32 +0200

    add user sign in test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,13 +7,13 @@ describe "the signin process", type: :feature do
   end
 
   it "signs me in" do
-    visit '/sessions/new'
-    within("#session") do
+    visit '/users/sign_in'
+    within(".form-box") do
       fill_in 'Email', with: 'user@example.com'
       fill_in 'Password', with: 'password'
     end
-    click_button 'Sign in'
-    expect(page).to have_content 'Success'
+    click_button 'Log in'
+    expect(page).to have_content "Signed in successfully."
   end
 end
 end
